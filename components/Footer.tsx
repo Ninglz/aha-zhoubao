bb2c1ca6b554eb4ba8104f06693138d399ffa791
ninglz <ninglz@yeah.net>
2023-03-24 18:53:54 +0800

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,9 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Footer() {
+
   return (
+
     <footer className='text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3'>
       <div>
         基于人工智能{' '}
@@ -31,5 +33,6 @@ export default function Footer() {
         </Link>
       </div>
     </footer>
+
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,30 @@ import '../styles/markdown.css'
 import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const getAnalyticsTag = () => {
+    return {
+      __html: `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?2e6aade1cd76dafd574ede5a7c5410bb";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();`,
+    }
+  }
   return (
-    <NextIntlProvider messages={pageProps.messages}>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-      <Analytics />
-    </NextIntlProvider>
+    <>
+    <script dangerouslySetInnerHTML={getAnalyticsTag()}/>
+
+      <NextIntlProvider messages={pageProps.messages}>
+        <ThemeProvider attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>
+        <Analytics />
+      </NextIntlProvider>
+    </>
   )
 }
 
-export default MyApp
+export default MyApp;
